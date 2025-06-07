7eeffeab0d228d21d78d6b882c173b9b09fd0af7
lina98st <alina29st@gmail.com>
2025-06-07 15:57:31 +0200

    Complete part 4, exercise 4.23

---
+++ part4/bloglist/app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const userExtractor = require('./middleware/userExtractor')
+
+
+const tokenExtractor = require('./middleware/tokenExtractor')
+
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error.message)
+    })
+
+app.use(express.json())
+app.use(tokenExtractor)
+
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+
+module.exports = app
+
+
+
+
---
+++ part4/bloglist/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    name: String,
+    passwordHash: { type: String, required: true },
+    blogs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Blog'
+        }
+    ]
+})
+
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
